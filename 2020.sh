dbdc0a75c396287748f914dada7eac93c0b73eb9
Mr Bell <54947269+bellattckr@users.noreply.github.com>
2020-01-01 11:28:39 +0800

    Update 2020.sh

--- 2020.sh
+++ 2020.sh
@@ -15,28 +15,19 @@ echo   ""
 echo -e "${red}SELAMAT DATANG DI PROGRAM PENGHAPUSAN KENANGAN"
 echo -e "${yellow}KAMI AKAN MEMBANTU ANDA MENGHAPUS KENANGAN DI 2019"
 
-import random
-import sys
-import time
-def mengetik(s):
-    for c in s + "\n":
-        sys.stdout.write(c)
-        sys.stdout.flush()
-
-        time.sleep(random.random() * 0.5)
-mengetik("\033[1;36m MENJALANKAN PROGRAM...")
-
+sleep 0.5
+echo   "SELAMAT DATANG DI PROGRAM PENGHAPUSAN KENANGAN"
 clear
 
 echo -e "${red}TULIS KENANGAN YANG MAU DI HAPUS"
 read kenangan
 if [ "$kenangan" == "KENANGAN DENGAN MANTAN" ]; then
   echo "MAAF PROSES PENGHAPUSAN GAGAL"
-   time.sleep(random.random() * 0.1)
+   sleep 0.1
   echo "SOALNYA KAMU MASIH MENCINTAINYA" 
-   time.sleep(random.random() * 0.1)
-   echo "SILAHKAN BERUSAHA BERHENTI MENCINTAINYA DULU" 
-   time.sleep(random.random() * 0.1) 
-   echo "LALU COBA LAGI" 
-   time.sleep(random.random() * 0.1) 
+   sleep 0.1
+  echo "SILAHKAN BERUSAHA BERHENTI MENCINTAINYA DULU" 
+   sleep 0.1
+  echo "LALU COBA LAGI" 
+   sleep 0.1
 fi
